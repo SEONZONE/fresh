d4ce15e2bb136a8c498a1ba0ddf47d2c23a6af44
seonzone <tjdwhd2247@naver.com>
2025-03-05 00:36:44 +0900

    feat: [Component에 데이터 전해주려면 props] 수업내용

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,24 +1,37 @@
 import {age,name} from './data.js'
-import HiComponent from './hi.js'
-
+let 장바구니 = ['Tomatoes','Pasta']
 export default function Cart() {
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <CartItem/>
-      <HiComponent/>
+      <CartItem items={장바구니[0]}/>
+      <CartItem items={장바구니[1]}/>
+      <Banner content="현대카드"/>
+      <Banner content="삼성카드"/>
+      <Btn color="red"/>
+      <Btn color="blue"/>
     </div>
   );
 }
 
-function CartItem() {
+function CartItem(props) {
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      <p>{props.items}</p>
       <p>$40</p>
       <p>1개</p>
       <p>나이: {age}</p>
       <p>이름: {name}</p>
     </div>
   );
+}
+
+function Banner(props){
+  return (
+    <h5> {props.content} 결제 행사중</h5>
+  )
+}
+
+function Btn(props){
+  return <button style={{ background : `${props.color}` }}>버튼임</button>
 }
